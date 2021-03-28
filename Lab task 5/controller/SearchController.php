d9499cd8e057bd170ce3166e6b878d377b68a874
sabihahazera <61106706+sabihahazera@users.noreply.github.com>
2021-03-28 22:25:42 +0600

    Add files via upload

---
+++ Lab task 5/controller/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../model/model.php';
+
+if (isset($_POST['search'])) {
+	
+		//echo $_POST['Product_name'];
+
+    try {
+    	
+    	$allSearchedProducts = SearchProducts($_POST['ProductName']);
+    	require_once '../SearchAllProducts.php';
+
+    } catch (Exception $ex) {
+    	echo $ex->getMessage();
+    }
+}
+
